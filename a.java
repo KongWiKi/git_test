4013ccfe2a2e2841604034cc789a60361c2348ec
weikunkun <kongwiki@163.com>
2020-10-19 21:53:02 +0800

    add a.java

---
+++ a.java
@@ -0,0 +1,12 @@
+public class a {
+    public void gen(int a) {
+        if(a/3 == 0) {
+            // do something
+        }else {
+            // do another
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
