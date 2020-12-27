81e5e6ec283b5cd68d8b52c0c6ecfa2f7d595376
weikunkun <kongwiki@163.com>
2020-12-27 15:41:46 +0800

    add say()

---
+++ Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author kongwiki@163.com
+ * @since 2020/12/27
+ */
+public class Test {
+    // 任务A
+    public void say() {
+        System.out.println("aaa");
+        // TODO
+        System.out.println("ccc");
+        System.out.println("ddd");
+        System.out.println("ddd");
+        System.out.println("ddd");
+    }
+}
